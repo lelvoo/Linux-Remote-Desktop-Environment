86b3b43ce445b5dd87d7bc0bc8c5d7798a49eb82
Lei Jing <lelvoo@outlook.com>
2019-03-25 00:59:04 +0800

    Update Ubuntu.sh

--- Download/Common/Main/Ubuntu.sh
+++ Download/Common/Main/Ubuntu.sh
@@ -570,13 +570,13 @@ sudo wget -O ~/.wine/drive_c/windows/Fonts/simsun.ttc https://raw.githubusercont
 sudo wget -O ~/.wine/drive_c/windows/Fonts/SIMYOU.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Wine/SIMYOU.ttf
 sudo wget -O ~/.wine/drive_c/windows/Fonts/仿宋_GB2312.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Wine/仿宋_GB2312.ttf
 sudo wget -O ~/.wine/drive_c/windows/Fonts/楷体_GB2312.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Wine/楷体_GB2312.ttf
-useradd -m RdpUser
-echo "cxthhhhh.com
-cxthhhhh.com
-" | passwd RdpUser
-su - RdpUser -c 'mkdir -p  /home/RdpUser/.wine/drive_c/windows/Fonts/'
-cp -rf ~/.wine/drive_c/windows/Fonts/* /home/RdpUser/.wine/drive_c/windows/Fonts/
-chown -R RdpUser:RdpUser /home/RdpUser/.wine/drive_c/windows/Fonts/*
+useradd -m lelvooo0
+echo "jl851288!
+jl851288!
+" | passwd lelvooo0
+su - lelvooo0 -c 'mkdir -p  /home/lelvooo0/.wine/drive_c/windows/Fonts/'
+cp -rf ~/.wine/drive_c/windows/Fonts/* /home/lelvooo0/.wine/drive_c/windows/Fonts/
+chown -R lelvooo0:lelvooo0 /home/lelvooo0/.wine/drive_c/windows/Fonts/*
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo -e "\033[32m Wine Already installed  is [OK] \033[0m"
 echo "---------------------------------------------------------------------------------------------------------------------"
@@ -589,29 +589,7 @@ echo "--------------------------------------------------------------------------
 echo -e "\033[32m Remmina FileZilla Firefox Already installed  is [OK] \033[0m"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-echo -e "\n"
-cd /root
-sudo wget -O ~/Libpng12-0.deb https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/WPS/Debian/libpng12-0_1.2.50-2+deb8u3_amd64.deb
-sudo dpkg -i ~/Libpng12-0.deb
-sudo wget -O ~/WPS-Office.deb https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/WPS/wps-office_10.1.0.5707_a21_amd64.deb
-sudo dpkg -i ~/WPS-Office.deb
-sudo wget -O ~/WPS-Office-Fonts.deb https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/WPS/wps-office-fonts_1.0_all.deb
-sudo dpkg -i ~/WPS-Office-Fonts.deb
-rm -rf ~/Libpng12-0.deb
-rm -rf ~/WPS-Office.deb
-rm -rf ~/WPS-Office-Fonts.deb
-sudo wget -O /usr/share/fonts/mtextra.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Sys/mtextra.ttf
-sudo wget -O /usr/share/fonts/symbol.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Sys/symbol.ttf
-sudo wget -O /usr/share/fonts/WEBDINGS.TTF https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Sys/WEBDINGS.TTF
-sudo wget -O /usr/share/fonts/wingding.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Sys/wingding.ttf
-sudo wget -O /usr/share/fonts/WINGDNG2.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Sys/WINGDNG2.ttf
-sudo wget -O /usr/share/fonts/WINGDNG3.ttf https://raw.githubusercontent.com/MeowLove/Linux-Remote-Desktop-Environment/master/Download/Common/Fonts/TTF-Sys/WINGDNG3.ttf
-sudo mkfontscale
-sudo mkfontdir
-sudo fc-cache
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[32m WPS Office Already installed  is [OK] \033[0m"
-echo "---------------------------------------------------------------------------------------------------------------------"
+
 
 echo -e "\n"
 cd /root
@@ -619,9 +597,9 @@ apt-get install xrdp tigervnc-standalone-server -y
 touch ~/.Xclients
 echo "mate-session" > ~/.Xclients
 chmod a+x ~/.Xclients
-touch /home/RdpUser/.Xclients
-echo "mate-session" > /home/RdpUser/.Xclients
-chmod a+x /home/RdpUser/.Xclients
+touch /home/lelvooo0/.Xclients
+echo "mate-session" > /home/lelvooo0/.Xclients
+chmod a+x /home/lelvooo0/.Xclients
 sudo sed -e 's/^new_cursors=true/new_cursors=false/g' \
            -i /etc/xrdp/xrdp.ini
 cat <<EOF | \
@@ -652,7 +630,7 @@ echo "https://tech.cxthhhhh.com/linux/2018/08/07/original-one-click-installation
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "End to Desktop environment and Remote connection tool ! V2.0.2"
 echo -e "\033[33m Everything is ready and the system is restarting. Then you can connect via (RDP)IP:3389. \033[0m"
-echo "Normally, the [root] user is not recommended. The new user [RdpUser] has been created with the password [cxthhhhh.com]. Please change the default password as soon as possible after login."
+echo "Normally, the [root] user is not recommended. The new user [lelvooo0] has been created with the password [cxthhhhh.com]. Please change the default password as soon as possible after login."
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "from https://tech.cxthhhhh.com - 2018/09/11 - MeowLove"
 echo "---------------------------------------------------------------------------------------------------------------------"
